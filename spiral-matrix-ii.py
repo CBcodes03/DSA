f99b0fd69ff32acca539565e2359fe9ad179a670
ChiragBudakoti <86680251+CBcodes03@users.noreply.github.com>
2026-01-15 01:30:53 +0530

    Implement spiral matrix generation in Python

---
+++ spiral-matrix-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0]*n for _ in range(n)]
+        top,left=0,0
+        right,bottom=n-1,n-1
+        start=1
+        while left <= right and top <= bottom:
+            for col in range(left,right+1):
+                matrix[top][col] = start
+                start+=1
+            top+=1
+            for row in range(top,bottom+1):
+                matrix[row][right] = start
+                start+=1
+            right-=1
+            for col in range(right,left-1,-1):
+                matrix[bottom][col] = start
+                start+=1
+            bottom-=1
+            for row in range(bottom,top-1,-1):
+                matrix[row][left] = start
+                start+=1
+            left+=1
+        return matrix
